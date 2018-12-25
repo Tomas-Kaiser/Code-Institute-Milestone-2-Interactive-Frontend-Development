0296c3fc161771e9457c2359b0fadcd02e42b4e0
Tomas Kaiser <tomas.kaiser06@gmail.com>
2018-12-25 14:51:21 +0000

    Getting the value from input radio

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,44 +1,41 @@
-getVenues();
 
-function getMap(dataAPI) {
 
-   var mymap = L.map('map').setView([50.058362, 14.454384], 12);
+   let cityName = null;
+   let querySelected = null;
 
-   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox.streets',
-      accessToken: 'pk.eyJ1IjoidG9tYXMta2Fpc2VyIiwiYSI6ImNqaWsxcXBlZjFzYXUzcG43d3Z3dzBnengifQ.mQDUjX4MQ49QWM-Yz4u19g'
-   }).addTo(mymap);
-   
-   var labels = "ABCDEFGHIJKLMNOPQRSTWXYZ" // It will appear on the makers
-
-   dataAPI.map( myVenue => {
-      var marker = L.marker([myVenue.venue.location.lat, myVenue.venue.location.lng]).addTo(mymap);
-      marker.bindPopup(`
-         <h4>${myVenue.venue.name}</h4>
-         <p>Adress of venue:</p>
-         <ul>
-            <li>${myVenue.venue.location.address}</li>
-            <li>${myVenue.venue.location.city}</li>
-            <li>${myVenue.venue.location.country}</li>
-         </ul>
-      `);
+   let radio = $("input[type='radio']");
+   radio.change(function() {
+      let filteredRadio = radio.filter(":checked");
+      querySelected = filteredRadio.val();
+      console.log(querySelected)
    });
-}
 
+   $("#submitCity").on("click", (e) => {
+     e.preventDefault();
+     cityName = $("#inputCity").val();
+
+      $("#map").remove();
+      $("#container-map").append('<div id="map" height="100px" width="100px"></div>');
+
+     getVenues();
+
+     console.log(cityName)
+   })
+
+   getVenues();
 
 // Foursquer API
+
 function getVenues() {
 
    const endPoint = "https://api.foursquare.com/v2/venues/explore?";
    const client_id = "FHYZP5IIDLYMMWGXEGXQU0SDZANGGKMEIU1ZWFUCOINVQFWT";
    const client_secret = "350EQKKN5RFDLJEEMMRLQZ2ESRVNQUAEB2EAR2PYZWQWWPUF";
-   const query = "food";
-   const near = "prague";
+   const query = querySelected;
+   const near = cityName;
    const v = 20180323;
 
-  fetch(`https://api.foursquare.com/v2/venues/explore?client_id=FHYZP5IIDLYMMWGXEGXQU0SDZANGGKMEIU1ZWFUCOINVQFWT&client_secret=350EQKKN5RFDLJEEMMRLQZ2ESRVNQUAEB2EAR2PYZWQWWPUF&v=20180323&limit=20&near=${near}&query=coffee`)
+  fetch(`${endPoint}client_id=${client_id}&client_secret=${client_secret}&v=${v}&limit=20&near=${near}&query=${query}`)
     .then(function(response) {
         // Code for handling API response
         return response.json();
@@ -55,4 +52,35 @@ function getVenues() {
     });
 }
 
+// Rendering the map
 
+function getMap(dataAPI) {
+
+   // if value in input is empty, then set view Europe, else the selected city
+   if (cityName === null) {
+      var mymap = L.map('map').setView([50.058362, 14.454384], 5);
+   } else {
+      var mymap = L.map('map').setView([dataAPI[0].venue.location.lat, dataAPI[0].venue.location.lng], 8);
+   }
+   
+
+   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoidG9tYXMta2Fpc2VyIiwiYSI6ImNqaWsxcXBlZjFzYXUzcG43d3Z3dzBnengifQ.mQDUjX4MQ49QWM-Yz4u19g'
+   }).addTo(mymap);
+
+   dataAPI.map( myVenue => {
+      var marker = L.marker([myVenue.venue.location.lat, myVenue.venue.location.lng]).addTo(mymap);
+      marker.bindPopup(`
+         <h4>${myVenue.venue.name}</h4>
+         <p>Adress of venue:</p>
+         <ul>
+            <li>${myVenue.venue.location.address}</li>
+            <li>${myVenue.venue.location.city}</li>
+            <li>${myVenue.venue.location.country}</li>
+         </ul>
+      `);
+   });
+}
